dbda7dc11f69c28bcdb73a9724c68d4fef2c5eb9
Teklewoyn Erdachew <99509917+teklewoyn1@users.noreply.github.com>
2022-03-16 04:38:52 -0700

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,22 +4,12 @@
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
-void positive_or_negative(int n)
-{
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-}
 
-
-int main()
+int main(void)
 {
         int i;
 
-        i = 0;
+        i = 98;
         positive_or_negative(i);
 
         return (0);
