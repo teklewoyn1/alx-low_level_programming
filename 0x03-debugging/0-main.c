b5bfde9ead7c10e76830db43c6792d45f2c38558
Teklewoyn Erdachew <99509917+teklewoyn1@users.noreply.github.com>
2022-03-15 23:53:47 -0700

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,10 +7,9 @@
 
 int main(void)
 {
-        int i;
-
-        i = 0;
-        positive_or_negative(int i);
+        srand(time(0));
+	n = rand() - RAND_MAX / 2;
+        positive_or_negative(n);
 
         return (0);
 }
