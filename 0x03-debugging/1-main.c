853d1703e82c268abfb37a3177649fe1368f16ae
Teklewoyn Erdachew <99509917+teklewoyn1@users.noreply.github.com>
2022-03-16 02:50:11 -0700

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -14,6 +14,7 @@
         {
                 if(i==0){
                   printf("Infinite loop avoided! \\o/\n");
+                  break;
                 }else{
                     putchar(i);    
                 }
